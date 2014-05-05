a846ed8d082b510b8e9818eb5d80f9a1c46d7be0
yitznewton <yitznewton@hotmail.com>
2014-05-05 08:34:29 -0400

    implement KeyboardInput

--- src/Yitznewton/TwentyFortyEight/Board.php
+++ src/Yitznewton/TwentyFortyEight/Board.php
@@ -4,5 +4,8 @@ namespace Yitznewton\TwentyFortyEight;
 
 class Board
 {
-
+    const MOVE_UP = 1;
+    const MOVE_DOWN = 2;
+    const MOVE_LEFT = 3;
+    const MOVE_RIGHT = 4;
 }
---
+++ src/Yitznewton/TwentyFortyEight/Input/KeyboardInput.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Yitznewton\TwentyFortyEight\Input;
+
+use Yitznewton\TwentyFortyEight\Board;
+
+class KeyboardInput implements Input
+{
+    private $originalTerminalSettings;
+
+    public function __construct()
+    {
+        $this->originalTerminalSettings = `stty -g`;
+        system('stty -icanon');
+    }
+
+    public function __destruct()
+    {
+        system("stty '" . $this->originalTerminalSettings . "'");
+    }
+
+    /**
+     * @throws \UnexpectedValueException
+     * @return mixed
+     */
+    public function getMove()
+    {
+        $char = $this->getCharFromKeyboard();
+
+        switch (strtolower($char)) {
+            case 'w':
+                return Board::MOVE_UP;
+            case 'a':
+                return Board::MOVE_LEFT;
+            case 's':
+                return Board::MOVE_DOWN;
+            case 'd':
+                return Board::MOVE_RIGHT;
+            default:
+                throw new \UnexpectedValueException('Invalid character: ' . $char);
+        }
+    }
+
+    /**
+     * @return int
+     */
+    private function getCharFromKeyboard()
+    {
+        return fread(STDIN, 1);
+    }
+}
