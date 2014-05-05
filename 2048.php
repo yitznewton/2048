5e8e0fc5e1bbb0d2977b20b149fb010e438d5353
yitznewton <yitznewton@hotmail.com>
2014-05-05 07:02:23 -0400

    initial commit: main loop and gitkeeps

---
+++ 2048.php
@@ -0,0 +1,14 @@
+<?php
+
+$input = new KeyboardInput();
+$output = new ConsoleOutput();
+
+$board = new Board();
+$board->initialize();
+$output->render($board);
+
+while ($board->hasEmptySlots()) {
+    $move = $input->getMove();
+    $board->addMove($move);
+    $output->render($board);
+}
