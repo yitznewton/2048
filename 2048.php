85b0d3837c29b8d8961db4a52385fa37163fe71c
yitznewton <yitznewton@hotmail.com>
2014-05-05 09:03:02 -0400

    add `use` statements to main

--- 2048.php
+++ 2048.php
@@ -1,5 +1,9 @@
 <?php
 
+use Yitznewton\TwentyFortyEight\Board;
+use Yitznewton\TwentyFortyEight\Input\KeyboardInput;
+use Yitznewton\TwentyFortyEight\Output\ConsoleOutput;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 $input = new KeyboardInput();
