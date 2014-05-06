bf8efabf13d3ee256356f4ea5bdae6fd88323a0e
yitznewton <yitznewton@hotmail.com>
2014-05-06 14:32:49 -0400

    handle unrecognized input in main loop

--- 2048.php
+++ 2048.php
@@ -2,6 +2,7 @@
 
 use Yitznewton\TwentyFortyEight\Board;
 use Yitznewton\TwentyFortyEight\Input\ConsoleKeyboardInput;
+use Yitznewton\TwentyFortyEight\Input\UnrecognizedInputException;
 use Yitznewton\TwentyFortyEight\Output\ConsoleOutput;
 
 require_once __DIR__ . '/vendor/autoload.php';
@@ -14,7 +15,13 @@ $board->initialize();
 $output->renderBoard($board);
 
 while ($board->hasPossibleMoves()) {
-    $move = $input->getMove();
+    try {
+        $move = $input->getMove();
+    } catch (UnrecognizedInputException $e) {
+        // ignore this input, and listen again
+        continue;
+    }
+
     $board->addMove($move);
     $output->renderBoard($board);
 }
