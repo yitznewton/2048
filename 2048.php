a75f41b8e5072ee7007423d0adf11896c891d7d6
yitznewton <yitznewton@hotmail.com>
2014-05-06 23:18:15 -0400

    update main file with new class structure

--- 2048.php
+++ 2048.php
@@ -1,16 +1,30 @@
 <?php
 
-use Yitznewton\TwentyFortyEight\Board;
-use Yitznewton\TwentyFortyEight\Input\ConsoleKeyboardInput;
+use Yitznewton\TwentyFortyEight\BoardImplementation;
+use Yitznewton\TwentyFortyEight\Input\Device\KeyboardInputDevice;
+use Yitznewton\TwentyFortyEight\Input\MappedInput;
 use Yitznewton\TwentyFortyEight\Input\UnrecognizedInputException;
+use Yitznewton\TwentyFortyEight\Move;
 use Yitznewton\TwentyFortyEight\Output\ConsoleOutput;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$input = new ConsoleKeyboardInput();
+$device = new KeyboardInputDevice();
+
+$input = new MappedInput([
+    'w' => Move::UP,
+    'a' => Move::LEFT,
+    's' => Move::DOWN,
+    'd' => Move::RIGHT,
+    'i' => Move::UP,
+    'j' => Move::LEFT,
+    'k' => Move::DOWN,
+    'l' => Move::RIGHT,
+], $device);
+
 $output = new ConsoleOutput();
 
-$board = new Board();
+$board = new BoardImplementation();
 $board->initialize();
 $output->renderBoard($board);
 
