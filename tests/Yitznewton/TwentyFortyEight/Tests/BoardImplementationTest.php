7b5dcd32c8a36c17dee06bf9a68983fa829850aa
yitznewton <yitznewton@hotmail.com>
2014-05-06 23:53:27 -0400

    rename BoardImplementationTest

--- tests/Yitznewton/TwentyFortyEight/Tests/BoardImplementationTest.php
+++ tests/Yitznewton/TwentyFortyEight/Tests/BoardImplementationTest.php
@@ -2,7 +2,7 @@
 
 namespace Yitznewton\TwentyFortyEight\Tests;
 
-class BoardTest extends \PHPUnit_Framework_TestCase
+class BoardImplementationTest extends \PHPUnit_Framework_TestCase
 {
     public static function setUpBeforeClass()
     {
