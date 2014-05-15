1337021564faa7a6fd6af1955bbd4c00c085627b
yitznewton <yitznewton@hotmail.com>
2014-05-14 23:53:31 -0400

    add MutedConsoleOutput for AI runs

--- ai_2048.php
+++ ai_2048.php
@@ -4,17 +4,15 @@ use Yitznewton\TwentyFortyEight\Game;
 use Yitznewton\TwentyFortyEight\Input\Artificial\ArtificialInputA;
 use Yitznewton\TwentyFortyEight\Input\DelayedProxyInput;
 use Yitznewton\TwentyFortyEight\Input\Device\KeyboardInputDevice;
-use Yitznewton\TwentyFortyEight\Input\MappedInput;
-use Yitznewton\TwentyFortyEight\Move\Move;
-use Yitznewton\TwentyFortyEight\Output\ConsoleOutput;
+use Yitznewton\TwentyFortyEight\Output\MutedConsoleOutput;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 $device = new KeyboardInputDevice();
 
 $aiInput = new ArtificialInputA();
-$delayedInput = new DelayedProxyInput($aiInput);
-$output = new ConsoleOutput();
+$delayedInput = new DelayedProxyInput($aiInput, 0);
+$output = new MutedConsoleOutput();
 
 $game = new Game(4, 2048, $delayedInput, $output);
 $game->run();
---
+++ src/Yitznewton/TwentyFortyEight/Output/MutedConsoleOutput.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Yitznewton\TwentyFortyEight\Output;
+
+use Yitznewton\TwentyFortyEight\Grid;
+
+class MutedConsoleOutput extends ConsoleOutput
+{
+    public function renderBoard(Grid $grid, $score)
+    {
+        // do nothing
+    }
+}
